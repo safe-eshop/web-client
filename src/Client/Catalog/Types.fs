3862574678bdd55f59096b015023e4c9fbfc8677
dominikus1993 <dominikus1910@gmail.com>
2019-07-23 23:07:41 +0200

    small chnages

--- src/Client/Catalog/Types.fs
+++ src/Client/Catalog/Types.fs
@@ -3,4 +3,5 @@ module Catalog.Types
 type Product = { id: string; name: string; description: string; price: float }
 
 type Msg =
-| GetAllProducts of page: int * pageSize: int
+| GetPagedCatalog of page: int * pageSize: int
+| PagedCatalogFullfiled of page: int * pageSize: int
