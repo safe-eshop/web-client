fdd4a5f39868fbec18ad1b40eb1616dbed2b8cf5
dominikus1993 <dominikus1910@gmail.com>
2020-01-11 22:58:57 +0100

    change product model

--- src/Catalog/Model.fs
+++ src/Catalog/Model.fs
@@ -1,27 +1,10 @@
 module Model 
 
-type ProductDetails = {
-    weight: int;
-    weightUnits: string;
-    manufacturer: string;
-    color: string;
-}
-
-type PictureUrl = string
-type Price = int
-type ProductId = string
-
-type ProductInfo = {
-    name: string;
-    slug: string;
-    rand: string;
-    description: string
-    picture: PictureUrl
-}
-
 type Product = {
-    id: ProductId;
-    info: ProductInfo;
-    details: ProductDetails;
-    price: Price;
+    id: string
+    name: string
+    slug: string
+    brand: string
+    description: string
+    picture: string
 }
